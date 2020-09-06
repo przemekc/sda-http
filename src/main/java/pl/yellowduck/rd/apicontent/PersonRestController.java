60f782418ff9d7b354cd97b6ad5e4a7c39f7c33a
Przemysław Chmielewski <przemekc83@gmail.com>
2020-09-06 14:43:41 +0200

    POST

--- src/main/java/pl/yellowduck/rd/apicontent/PersonRestController.java
+++ src/main/java/pl/yellowduck/rd/apicontent/PersonRestController.java
@@ -1,9 +1,14 @@
 package pl.yellowduck.rd.apicontent;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,6 +20,27 @@ public class PersonRestController {
     return List.of(
         new Person("Jan", "Kowalski"),
         new Person("Anna", "Nowak")
+    );
+  }
+
+  @PostMapping
+  public ResponseEntity<String> save(String string) {
+    return ResponseEntity.ok("OK");
+  }
+
+  @RequestMapping(value = "/cv", method = RequestMethod.GET)
+  public ResponseEntity<FileSystemResource> downloadStuff()
+      throws IOException {
+    File file = new File("/home/przemekc/cv.pdf");
+    long fileLength = file.length();
+
+    HttpHeaders respHeaders = new HttpHeaders();
+    respHeaders.setContentType(MediaType.APPLICATION_PDF);
+    respHeaders.setContentLength(fileLength);
+    respHeaders.setContentDispositionFormData("attachment", "cv.pdf");
+
+    return new ResponseEntity<FileSystemResource>(
+        new FileSystemResource(file), respHeaders, HttpStatus.OK
     );
   }
 }
